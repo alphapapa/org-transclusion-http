57992e1ba48bca6c501340632ba61110dad2f8c3
Joseph Turner <joseph@breatheoutbreathe.in>
2024-03-29 20:24:18 -0700

    Meta: Hack to remove html <head>

--- build-readme.el
+++ build-readme.el
@@ -1,4 +1,10 @@
 (require 'org)
+(require 'ox-html)
 (with-temp-buffer
   (insert-file-contents "README.org")
-  (org-export-to-file 'html "README.html"))
+  (cl-letf (((symbol-function 'org-html--build-meta-info)
+             ;; HACK: Sourcehut erroneously displays <head> content, so
+             ;; we stop org from generating <head> entirely.
+             ;; TODO: File bug with Sourcehut to stop from rendering <head>
+             #'ignore))
+    (org-export-to-file 'html "README.html")))
