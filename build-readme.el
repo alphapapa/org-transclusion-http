2f028aff5d2e040984bf35d04c259865651b1388
Joseph Turner <joseph@breatheoutbreathe.in>
2024-05-02 10:22:21 -0400

    Meta: (README.org) Just export HTML body
    In this
    message (<https://lists.sr.ht/~sircmpwn/sr.ht-discuss/%3C87sez21jhj.fsf@gilgamesh.mail-host-address-is-not-set%3E#%3C72b38ae4-250f-4c8b-a894-56e685addd3f@bitfehler.net%3E>),
    Conrad Hoffman points out that the SourceHut renderer expects an
    HTML *snippet*, not a whole document.
    
    Suggested-by: Conrad Hoffman <ch@bitfehler.net>

--- build-readme.el
+++ build-readme.el
@@ -2,9 +2,4 @@
 (require 'ox-html)
 (with-temp-buffer
   (insert-file-contents "README.org")
-  (cl-letf (((symbol-function 'org-html--build-meta-info)
-             ;; HACK: Sourcehut erroneously displays <head> content, so
-             ;; we stop org from generating <head> entirely.
-             ;; TODO: File bug with Sourcehut to stop from rendering <head>
-             #'ignore))
-    (org-export-to-file 'html "README.html")))
+  (org-export-to-file 'html "README.html" nil nil nil t))
